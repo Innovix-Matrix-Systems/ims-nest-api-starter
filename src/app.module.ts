a61747dac0ddfa48ce32dadfbf5932fb29a9d24c
Md Azizul Hakim <mdazizulhakim.cse@gmail.com>
2024-10-19 18:34:34 +0600

    add throttler rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { CommandModule } from 'nestjs-command';
 import { AppController } from './app.controller';
 import { XSecureInstallCommand } from './commands/xsecurity.command';
@@ -22,9 +24,19 @@ import { UserModule } from './modules/user/user.module';
     }),
     MikroOrmModule.forRootAsync({
       imports: [ConfigModule],
+      inject: [ConfigService],
       useFactory: (configService: ConfigService) =>
         mikroOrmConfig(configService),
+    }),
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
       inject: [ConfigService],
+      useFactory: (configService: ConfigService) => [
+        {
+          ttl: configService.get<number>('THROTTLE_TTL'),
+          limit: configService.get<number>('THROTTLE_LIMIT'),
+        },
+      ],
     }),
     CommandModule,
     HealthModule,
@@ -35,9 +47,14 @@ import { UserModule } from './modules/user/user.module';
     AuthModule,
   ],
   controllers: [AppController],
-  providers: [XSecureInstallCommand],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+    XSecureInstallCommand,
+  ],
 })
-// export class AppModule {}
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer.apply(XSecurityMiddleware).forRoutes('*');
