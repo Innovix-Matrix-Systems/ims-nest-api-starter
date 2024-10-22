26b9fea6b7124bc3ff832991bd498b7556519269
amanullah7649 <aullah060@gmail.com>
2024-10-22 21:57:42 +0600

    Added position decorator to get the module name

--- src/commands/create-module.command.ts
+++ src/commands/create-module.command.ts
@@ -6,18 +6,20 @@ import * as path from 'path';
 
 @Injectable()
 export class CreateModuleCommand {
+
   @Command({
     command: 'create:module <moduleName>',
     describe: 'Create a new module with a predefined structure',
   })
+
   async run(
+
     @Positional({
       name: 'moduleName',
       describe: 'The name of the module',
       type: 'string',
     })
-    moduleName: string
-
+    moduleName: string,
   ): Promise<void> {
 
     if (!moduleName) {
@@ -47,8 +49,10 @@ export class CreateModuleCommand {
 
   // Utility function to run shell commands
   runCommand(command: string): void {
+
     try {
       execSync(command, { stdio: 'inherit' });
+
     } catch (error) {
       console.error(`Error executing command: ${command}`, error);
     }
@@ -56,9 +60,11 @@ export class CreateModuleCommand {
 
   // Utility function to create a directory if it doesn't exist
   createDir(dirPath: string): void {
+
     if (!fs.existsSync(dirPath)) {
       fs.mkdirSync(dirPath, { recursive: true });
       console.log(`Created directory: ${dirPath}`);
+
     } else {
       console.log(`Directory already exists: ${dirPath}`);
     }
@@ -66,9 +72,12 @@ export class CreateModuleCommand {
 
   // Utility function to create a file with content
   createFile(filePath: string, content: string): void {
+
     if (!fs.existsSync(filePath)) {
+
       fs.writeFileSync(filePath, content);
       console.log(`Created file: ${filePath}`);
+
     } else {
       console.log(`File already exists: ${filePath}`);
     }
