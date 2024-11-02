ad6476ad63641030926e6841425f22c8b36cb3e2
Md Azizul Hakim <mdazizulhakim.cse@gmail.com>
2024-11-03 01:38:05 +0600

    added xescurity integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,18 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { RedisModule } from '@nestjs-modules/ioredis';
-import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { BullModule } from '@nestjs/bullmq';
+import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { CommandModule } from 'nestjs-command';
+import { XSecurityModule } from 'nestjs-xsecurity';
 import { AppController } from './app.controller';
 import { CreateModuleCommand } from './commands/create-module.command';
-import { XSecureInstallCommand } from './commands/xsecurity.command';
 import bullRedisConfig from './config/bull-redis.config';
 import mikroOrmConfig from './config/mikro-orm.config';
 import redisConfig from './config/redis.config';
-import { XSecurityMiddleware } from './middlewares/xsecurity.middleware';
+import xsecurityConfig from './config/xsecurity.config';
 import { AuthModule } from './modules/auth/auth.module';
 import { CacheModule } from './modules/cache/cache.module';
 import { EmailModule } from './modules/email/email.module';
@@ -20,7 +21,6 @@ import { MiscModule } from './modules/misc/misc.module';
 import { PermissionModule } from './modules/permission/permission.module';
 import { RoleModule } from './modules/role/role.module';
 import { UserModule } from './modules/user/user.module';
-import { BullModule } from '@nestjs/bullmq';
 
 @Module({
   imports: [
@@ -55,8 +55,18 @@ import { BullModule } from '@nestjs/bullmq';
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
         connection: bullRedisConfig(configService),
-        defaultJobOptions: { attempts: 3, removeOnComplete: true },
+        defaultJobOptions: {
+          attempts: 3,
+          removeOnComplete: true,
+          removeOnFail: true,
+        },
       }),
+    }),
+    XSecurityModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        xsecurityConfig(configService),
     }),
     CommandModule,
     HealthModule,
@@ -74,12 +84,7 @@ import { BullModule } from '@nestjs/bullmq';
       provide: APP_GUARD,
       useClass: ThrottlerGuard,
     },
-    XSecureInstallCommand,
     CreateModuleCommand,
   ],
 })
-export class AppModule implements NestModule {
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(XSecurityMiddleware).forRoutes('*');
-  }
-}
+export class AppModule {}
