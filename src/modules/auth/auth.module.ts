c8e4ef16aa2069b94687b1200962ebc75524e5a6
Md Azizul Hakim <mdazizulhakim.cse@gmail.com>
2024-12-21 14:02:43 +0600

    fix token expiration timing issue

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { EmailModule } from '../email/email.module';
 import { UserModule } from '../user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { JwtStrategy } from './strategies/jwt.strategy';
-import { PassportModule } from '@nestjs/passport';
 import { GoogleStrategy } from './strategies/google.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
@@ -17,7 +17,8 @@ import { GoogleStrategy } from './strategies/google.strategy';
       useFactory: async (configService: ConfigService) => ({
         secret: configService.get<string>('JWT_SECRET'),
         signOptions: {
-          expiresIn: configService.get<string>('JWT_EXPIRATION') || '30d',
+          expiresIn:
+            Number(configService.get<string>('JWT_EXPIRATION')) || '30d',
         },
       }),
       inject: [ConfigService],
